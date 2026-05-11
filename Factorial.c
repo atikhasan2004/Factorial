914966d0b4e8aebc060de593427299851fc63804
Atik Hasan <saatikhasan5021@gmail.com>
2026-05-11 22:52:13 +0600

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,num,fact=1;
+    printf()
+    for (i=1;i<=10;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
